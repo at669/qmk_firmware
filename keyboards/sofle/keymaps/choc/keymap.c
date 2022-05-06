7c008d83bdaceb201350242f6419de8cfa7c3e29
Amanda Trang <at669@cornell.edu>
2022-05-05 23:08:31 -0700

    Arrow key layer

--- keyboards/sofle/keymaps/choc/keymap.c
+++ keyboards/sofle/keymaps/choc/keymap.c
@@ -4,11 +4,11 @@
 enum sofle_layers {
     _QWERTYWIN = 0,
     _QWERTYMAC,
+    _GAME,
     _LOWER,
     _RAISE,
     _ADJUST,
 };
-
 
 #define LOWER  MO(_LOWER)
 #define RAISE  MO(_RAISE)
@@ -18,6 +18,7 @@ enum sofle_layers {
 enum custom_keycodes {
     KC_WIN = SAFE_RANGE,
     KC_MAC,
+    KC_GAME,
     KC_LOWER,
     KC_RAISE,
     KC_ADJUST,
@@ -84,6 +85,46 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTRL, KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_RCTRL, KC_RALT, KC_RGUI
         //            \--------+--------+--------+---------+-------|   |--------+---------+--------+---------+-------/
         ),
+    /*
+     * GAME
+     * ,-----------------------------------------.                    ,-----------------------------------------.
+     * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |RShift|
+     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+     * | GRV  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |  up  |   O  |   P  | Bspc |
+     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+     * | TAB  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  | left | down |Right |   ;  |  '   |
+     * |------+------+------+------+------+------| MPLY  |    | Caps  |------+------+------+------+------+------|
+     * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  -   |
+     * `-----------------------------------------/       /     \      \-----------------------------------------'
+     *            | LCTRL| LGUI | LALT | Lower| / Space /       \Enter \  |RAISE | RCTR | RALT | RGUI  |
+     *            |      |      |      |      |/       /         \      \ |      |      |      |      |
+     *            `----------------------------------'           '------''---------------------------'
+     */
+    [_GAME] = LAYOUT(
+        //,------------------------------------------------.                    ,---------------------------------------------------.
+        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, RSFT_T(KC_ENT),
+        //|------+-------+--------+--------+--------+------|                   |--------+-------+--------+--------+--------+---------|
+        KC_GRV, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_UP, KC_O, KC_P, KC_BSPC,
+        //|------+-------+--------+--------+--------+------|                   |--------+-------+--------+--------+--------+---------|
+        KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_LEFT, KC_DOWN, KC_RIGHT, KC_SCLN, KC_QUOT,
+        //|------+-------+--------+--------+--------+------|  ===  |   |  ===  |--------+-------+--------+--------+--------+---------|
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_MPLY, KC_CAPS, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_MINS,
+        //|------+-------+--------+--------+--------+------|  ===  |   |  ===  |--------+-------+--------+--------+--------+---------|
+        KC_LCTRL, KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_RCTRL, KC_RALT, KC_RGUI
+        //            \--------+--------+--------+---------+-------|   |--------+---------+--------+---------+-------/
+        ),
+    // //,------------------------------------------------.                    ,---------------------------------------------------.
+    // KC_ESC,   KC_1,    KC_2,     KC_3,    KC_4,   KC_5,                      KC_6,     KC_7,   KC_8,    KC_9,   KC_0,    RSFT_T(KC_ENT),
+    // //|------+-------+--------+--------+--------+------|                   |--------+-------+--------+--------+--------+---------|
+    // _______,   KC_Q,   KC_W,     KC_E,    KC_R,   KC_T,                      KC_Y,     KC_U,   KC_UP,   KC_O,   KC_P,    KC_BSPC,
+    // //|------+-------+--------+--------+--------+------|                   |--------+-------+--------+--------+--------+---------|
+    // _______,   KC_A,   KC_S,      KC_D,    KC_F,   KC_G,                       KC_H, KC_LEFT, KC_DOWN, KC_RGHT, KC_SCLN, KC_QUOT,
+    // //|------+-------+--------+--------+--------+------|  MUTE  |   |  ===  |--------+-------+--------+--------+--------+---------|
+    // KC_LSFT,   KC_Z,   KC_X,    KC_C,     KC_V,   KC_B, KC_MPLY,   KC_CAPS,    KC_N,   KC_M,   KC_COMM,  KC_DOT, KC_SLSH, KC_MINS
+    // //|------+-------+--------+--------+--------+------|  ===  |   |  ===  |--------+-------+--------+--------+--------+---------|
+    // KC_LCTRL, KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC,                   KC_ENT, MO(_RAISE), KC_RCTRL, KC_RALT, KC_RGUI
+    // //            \--------+--------+--------+---------+-------|   |--------+---------+--------+---------+-------/
+    // ),
     /* LOWER
      * ,-----------------------------------------.                    ,-----------------------------------------.
      * |      |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
@@ -145,7 +186,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
      * | DEBUG|      |      |      |      |      |                    |      |      |      |      |      | DEBUG|
      * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-     * |RGBTOG|bri up|hue up|sat up|spd up|      |-------.    ,-------|      |spd up|sat up|hue up|bri up|RGBTOG|
+     * |RGBTOG|bri up|hue up|sat up|spd up|      |-------.    ,-------| GAME |spd up|sat up|hue up|bri up|RGBTOG|
      * |------+------+------+------+------+------| trans |    | trans |------+------+------+------+------+------|
      * |RGBMOD|bri dn|hue dn|sat dn|spd dn| WIN  |-------|    |-------| MAC  |spd dn|sat dn|hue dn|bri dn|RGBMOD|
      * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -159,7 +200,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //|------+-------+--------+--------+--------+------|                   |--------+-------+--------+--------+--------+---------|
         DEBUG, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, DEBUG,
         //|------+-------+--------+--------+--------+------|                   |--------+-------+--------+--------+--------+---------|
-        RGB_TOG, RGB_VAI, RGB_HUI, RGB_SAI, RGB_SPI, KC_NO, KC_NO, RGB_SPI, RGB_SAI, RGB_HUI, RGB_VAI, RGB_TOG,
+        RGB_TOG, RGB_VAI, RGB_HUI, RGB_SAI, RGB_SPI, KC_NO, KC_GAME, RGB_SPI, RGB_SAI, RGB_HUI, RGB_VAI, RGB_TOG,
         //|------+-------+--------+--------+--------+------|  ===  |   |  ===  |--------+-------+--------+--------+--------+---------|
         RGB_MOD, RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD, KC_WIN, _______, _______, KC_MAC, RGB_SPD, RGB_SAD, RGB_HUD, RGB_VAD, RGB_MOD,
         //|------+-------+--------+--------+--------+------|  ===  |   |  ===  |--------+-------+--------+--------+--------+---------|
@@ -265,6 +306,9 @@ static void print_status_narrow(void) {
         case _QWERTYMAC:
             oled_write_ln_P(PSTR("mac"), false);
             break;
+        case _GAME:
+            oled_write_ln_P(PSTR("game"), false);
+            break;
     }
     // Print Layer state
     oled_write_P(PSTR("\n\n"), false);
@@ -274,6 +318,9 @@ static void print_status_narrow(void) {
             oled_write_ln_P(PSTR("qwert\n"), false);
             break;
         case _QWERTYMAC:
+            oled_write_ln_P(PSTR("qwert\n"), false);
+            break;
+        case _GAME:
             oled_write_ln_P(PSTR("qwert\n"), false);
             break;
         case _RAISE:
@@ -329,6 +376,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           set_single_persistent_default_layer(_QWERTYMAC);
         }
         return false;
+      case KC_GAME:
+          if (record->event.pressed) {
+              set_single_persistent_default_layer(_GAME);
+          }
+          return false;
       case KC_PRVWD:
         if (record->event.pressed) {
                 // if (keymap_config.swap_lctl_lgui) {
@@ -461,6 +513,7 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     switch (get_highest_layer(layer_state)) {
       case _QWERTYWIN:
       case _QWERTYMAC:
+      case _GAME:
         if (clockwise) {
           tap_code(KC_VOLD);
         } else {
@@ -481,6 +534,7 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
       switch (get_highest_layer(layer_state)) {
         case _QWERTYWIN:
         case _QWERTYMAC:
+        case _GAME:
           if (clockwise) {
             tap_code(KC_UP);
           } else {
